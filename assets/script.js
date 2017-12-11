4a644f0b9ee8f79c0b4c3819fa91df26f8c085d9
Tom <tomyip0010@hotmail.com>
2017-12-11 10:17:46 +0800

    Tom's part new update

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,73 @@
 $('.set-trip').pickmeup_twitter_bootstrap();
 
-$('#login-btn').on('click', (e) => {
-    $('#user-section').addClass("show-active")
+$(document).on('click', '#login-btn',(e) => {
+    $('#user-section').empty().html($(`
+        <span class="glyphicon glyphicon-remove close"></span>
+        <h3 class="form-title text-center">LOGIN</h3>
+        
+        <div class="text-center" id="social-login">
+            <a class="btn btn-primary" href="/auth/facebook" id="fb-login">f FACEBOOK</a>
+        </div>
+
+        <hr>
+
+        <form class="form-horizontal" name="login" method="POST" action="/login">   
+            <div class="form-group">
+                <label for="email" class="control-label">Email: </label>
+                <input type="email" name="username" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="password" class="control-label">Password: </label>
+                <input type="password" name="password" class="form-control">
+            </div>
+                
+            <div class="form-group text-center">
+                <input type="submit" value="Login" class="btn btn-default">
+            </div>
+        </form>
+        <a class="signup-btn">Not yet a member?Sign up here</a>
+    `))
+    if (!$('#user-section').hasClass("show-active")) {
+        $('#user-section').addClass("show-active")
+    }
+})
+
+$(document).on('click','.close', (e) => {
+    $('#user-section').removeClass("show-active")
+})
+
+$(document).on('click', '.signup-btn',(e) => {
+    $('#user-section').empty().html($(`
+        <span class="glyphicon glyphicon-remove close"></span>
+        <h3 class="form-title text-center">SIGN UP</h3>
+        <form class="form-horizontal" name="signup" method="POST" action="/user">
+            <div class="form-group">
+                <label for="username" class="control-label">Username: </label>
+                <input type="text" name="username" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="email" class="control-label">Email: </label>
+                <input type="email" name="email" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="password" class="control-label">Password: </label>
+                <input type="password" name="password" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="password" class="control-label">Password confirmation: </label>
+                <input type="password" name="password_confirmation" class="form-control">
+            </div>
+                
+            <div class="form-group text-center">
+                <input type="submit" value="Register" class="btn btn-default">
+            </div>
+        </form>
+    `))
+    if (!$('#user-section').hasClass("show-active")) {
+        $('#user-section').addClass("show-active")
+    }
 })

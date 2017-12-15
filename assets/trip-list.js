7cc31b643e2395c67c967634cc33aa7a36d4e340
Midori-Funaki <funamoru@gmail.com>
2017-12-15 17:18:01 +0800

    midori : added swappable on schedule container

---
+++ assets/trip-list.js
@@ -0,0 +1,8 @@
+
+const swappable = new Swappable.default(document.getElementsByClassName('schedule-activity-blocks')[0], {
+  draggable: 'li',
+});
+
+swappable.on('swappable:start', () => console.log('swappable:start'))
+swappable.on('swappable:swapped', () => console.log('swappable:swapped'));
+swappable.on('swappable:stop', () => console.log('swappable:stop'));

3de12b64dc1fe9fbeb4d5d4077d0fd0dd15b2448
Midori-Funaki <funamoru@gmail.com>
2017-12-16 21:15:09 +0800

    midori : can add new swappable box but there is bug

--- assets/trip-list.js
+++ assets/trip-list.js
@@ -8,14 +8,29 @@ swappable.on('swappable:stop', () => console.log('swappable:stop'));
 
 if($('#new-activity-type').text().length > 0){
   if($('#new-activity-type').text() === 'Hotel'){
-    console.log('new event is hotel..');
+    //console.log('new event is hotel..');
     let days = ['Mon','Tue','Wed','Thur','Fri','Sat','Sun'];
-    let newHotelNmae =  $('.new-activity-name').text();
+    let newHotelName =  $('.new-activity-name').text();
     let newHoteAddress = $('.new-activity-location').text();
     let newCheckIn = $('.new-acticity-hotel-check-in').text();
     let newCheckInDay = days[new Date(newCheckIn).getDay()-1];
     let newCheckOut = $('.new-acticity-hotel-check-out').text();
     let newCheckOutDay = days[new Date(newCheckOut).getDay()-1];
-    console.log('new check in is >>'+newCheckIn+'-'+newCheckInDay);
+    //console.log('new check in is >>'+newCheckIn+'-'+newCheckInDay);
+    let newSwappableActivity = `
+      <li class="list-group-item swappable-block">
+        <div class="row">
+            <div class="col-xs-8">
+                <h6>Hotel</h6>
+            </div>
+            <div class="col-xs-4">
+                <i class="fa fa-times fa-1x"></i>
+            </div>
+        </div>
+        <p>${newHotelName}</p>
+        <p>${newHoteAddress}</p>
+      </li>
+    `
+    $(`.activity-section-${newCheckIn}-${newCheckInDay}`).find('ul').append(newSwappableActivity);
   }
 }

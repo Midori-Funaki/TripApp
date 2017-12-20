a1a6a6d8c62be661843667af777fc9edee6e749c
Midori-Funaki <funamoru@gmail.com>
2017-12-20 21:10:08 +0800

    midori: adding delete function on schedule

--- assets/trip-list.js
+++ assets/trip-list.js
@@ -1,15 +1,33 @@
-let swappable;
-
-for(let i=0; i<document.getElementsByClassName('schedule-activity-blocks').length; i++){
-   swappable = new Swappable.default(document.getElementsByClassName('schedule-activity-blocks')[i], {
-    draggable: 'li',
-  });
-}
-
-swappable.on('swappable:start', () => console.log('swappable:start'))
-swappable.on('swappable:swapped', () => console.log('swappable:swapped'));
-swappable.on('swappable:stop', () => console.log('swappable:stop'));
-
+$(function() {
+  /*
+  $('.delete-activity').on('click',function(){
+    console.log('x clicked >>'+this);
+    $(this).find('.swappable-block').css('background-color','black');
+  })
+  */
+  
+  let swappable;
+  
+  for(let i=0; i<document.getElementsByClassName('schedule-activity-blocks').length; i++){
+     swappable = new Swappable.default(document.getElementsByClassName('schedule-activity-blocks')[i], {
+      draggable: 'li',
+    });
+    swappable.on('swappable:start', (e) => {
+      if ($(e.dragEvent.sensorEvent.target).hasClass('delete-activity')){
+        // we know the user clicked delete here
+        //console.log('e >>'+JSON.stringify(e));
+        //console.log('x clicked >>'+JSON.stringify($(e.dragEvent.sensorEvent.target)));
+        //console.log('closest ht >>'+JSON.stringify($(e.dragEvent.sensorEvent.target).find('h5')));
+        console.log(JSON.stringify('input content >>'+$(e.dragEvent.sensorEvent.target).children('.hidden-input-data')));
+        $(e.dragEvent.sensorEvent.target).parents('.list-group-item').empty();
+      }
+    });
+    swappable.on('swappable:swapped', () => console.log('swappable:swapped'));
+    swappable.on('swappable:stop', () => console.log('swappable:stop'));
+  }
+  
+  
+});
 /*
 let days,
     newHotelName,
